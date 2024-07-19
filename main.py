1e2799e00f6e19cef35f9fae61409dddb7abaff7
ksg-dev <ksg.dev.data@gmail.com>
2024-07-19 14:24:49 -0500

    get blog post page

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+from post import Post
 import requests
+
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+all_posts = requests.get(blog_url).json()
+post_objs = []
+
+for post in all_posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objs.append(post_obj)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", posts=post_objs)
+
+@app.route("/blog/<int:num>")
+def get_post(num):
+    target_post = None
+    for blog_post in post_objs:
+        if blog_post.id == num:
+            target_post = blog_post
+    return render_template("post.html", post=target_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
